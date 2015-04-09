ae3a2643a54b177bdfb2e47a091ffcd0f15216cb
Linka <Linka@zeoalliance.com>
2015-04-09 18:24:06 +0300

    -

--- src/main/CommandParser.java
+++ src/main/CommandParser.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.io.*;
+
 /**
  * Created by linka on 09.04.2015.
  */
@@ -9,4 +11,24 @@ public class CommandParser {
         return command.matches("tail -n\\d+\\s.+");
     }
 
+    public int fileLength(File file) throws IOException {
+        LineNumberReader lnr = new LineNumberReader(new FileReader(file));
+        lnr.skip(Long.MAX_VALUE);
+        int length = lnr.getLineNumber() + 1;
+        lnr.close();
+        return length;
+    }
+
+    public void printLines(File file, int startLine) throws IOException {
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            String line;
+            int lineNumber = 0;
+            while ((line = br.readLine()) != null) {
+
+                    System.out.println(line);
+
+                lineNumber++;
+            }
+        }
+    }
 }
